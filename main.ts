a3aaad45feab60a7c2969c3d0496469a8de405ee
kyqpham <kyqpham@gmail.com>
2025-04-28 19:46:15 -0400

    commiting final version

--- main.ts
+++ main.ts
@@ -1,134 +1,49 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Plugin } from 'obsidian';
+import * as Prism from 'prismjs';
+import 'prismjs/components/prism-javascript';
 
-// Remember to rename these classes and interfaces!
+export default class PseudoHighlightPlugin extends Plugin {
+  async onload() {
+    console.log('Loading PseudoHighlightPlugin...');
 
-interface MyPluginSettings {
-	mySetting: string;
-}
+    // Define the pseudocode language with a distinct token for method headers
+    Prism.languages.pseudo = {
+      comment: /\/\/.*|\/\*[\s\S]*?\*\//,
+      keyword: /\b(IF|ELSE|WHILE|FOR|FUNCTION|RETURN|PRINT|READ)\b/i,
+      boolean: /\b(TRUE|FALSE|AND|OR)\b/i,
+      string: /"(?:\\.|[^"])*"|\'(?:\\.|[^'])*'/,
+      number: /\b\d+\b/,
+      operator: /[-+*\/%=<>!]+/,
+      // Define a separate token specifically for method headers
+      methodHeader: {
+        pattern: /\b\w+\s*(?=\s*\()/, // Matches only method names followed by (
+        alias: 'methodHeader'
+      },
+      variable: /\b\w+\b/, // variables
+      punctuation: /[{}[\];(),.:]/
+    };
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+    // Register the markdown code block processor for pseudocode
+    this.registerMarkdownCodeBlockProcessor('pseudo', (source, el, ctx) => {
+      const pre = document.createElement('pre');
+      const code = document.createElement('code');
+      code.className = 'language-pseudo';
+      code.textContent = source;
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+      // Highlight the code using Prism
+      Prism.highlightElement(code);
 
-	async onload() {
-		await this.loadSettings();
+      // Apply inline styles to distinguish method headers without external CSS
+      code.innerHTML = code.innerHTML.replace(/<span class="token methodHeader">([^<]+)<\/span>/g, (match, methodName) => {
+        return `<span style="color: #1e90ff; font-weight: bold;">${methodName}</span>`; // Apply distinct color and style
+      });
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+      pre.appendChild(code);
+      el.appendChild(pre);
+    });
+  }
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+  onunload() {
+    console.log('Unloading PseudoHighlightPlugin...');
+  }
 }
